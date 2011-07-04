baeb0f337650902d198e08be5d539dc715f84fca
Martin Westergaard Lassen <martin@mwl.dk>
2011-07-04 12:38:28 +0200

    Renamed proxy file

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-var port = process.env.C9_PORT || process.env.PORT || 5678;
+var port = process.env.VMC_APP_PORT || process.env.C9_PORT || process.env.PORT || 5678;
 require('zappa').run_file('./server.coffee', { port: [port]});
